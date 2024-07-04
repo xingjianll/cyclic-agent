06d184b384e80d95f3446cda940735c29fa2f893
xingjianll <4396kevinliu@gmail.com>
2024-07-04 08:27:14 -0400

    dev: upgrade to python 3.12

--- src/state.py
+++ src/state.py
@@ -1,62 +1,36 @@
 import inspect
-from typing import Generic, TypeVar, Callable
+from abc import abstractmethod
+from typing import Generic, TypeVar, Protocol
 
-A = TypeVar('A')
-S = TypeVar('S', bound='State')
+from pydantic import BaseModel
+
+SigT = TypeVar('SigT')
 
 
-class State(Generic[A, S]):
-    def __init__(self, state_input: A, transition_fn: Callable[[A], S] | None = None):
-        self.state_input = state_input
-        self.transition_fn = transition_fn
+class Endomorphic(Protocol):
+    """Endomorphism"""
+    def next(self: 'Endomorphic') -> 'Endomorphic':
+        ...
 
-    def evoke(self) -> S:
-        if self.transition_fn:
-            return self.transition_fn(self.state_input)
-        return self._evoke(self.state_input)
 
-    def _evoke(self, state_input: A) -> S:
+class State(Endomorphic, BaseModel, Generic[SigT]):
+    @abstractmethod
+    def next(self, signal: SigT | None = None) -> 'State':
+        """Transition to the next state."""
         raise NotImplementedError
 
-    def __call__(self, *args, **kwargs) -> S:
-        return self.evoke()
 
-
-def state(fn: Callable[[A], S]):
-    sig = inspect.signature(fn)
-
-    def wrapper(*args, **kwargs) -> State[A, S]:
-        bound_args = sig.bind(*args, **kwargs)
-        bound_args.apply_defaults()
-        if bound_args.arguments:
-            state_input = next(iter(bound_args.arguments.values()))
-        else:
-            raise ValueError("Function requires at least one argument, none provided")
-        a: State[A, S] = State(state_input, fn)
-        return a
-
-    return wrapper
-
-
-@state
-def func1(a: int) -> State[int | str, State]:
-    print(a)
-    if a == 2:
-        return func2('hi')
-    return func1(a + 1)
-
-
-@state
-def func2(a: str) -> State:
-    print(a)
-    return func1(0)
-
-
-def main():
-    state = func1(0)
-    for i in range(5):
-        state = state.evoke()
-
-
-if __name__ == "__main__":
-    main()
+# def state(fn: Callable[[A], S]):
+#     sig = inspect.signature(fn)
+#
+#     def wrapper(*args, **kwargs) -> State[A, S]:
+#         bound_args = sig.bind(*args, **kwargs)
+#         bound_args.apply_defaults()
+#         if bound_args.arguments:
+#             state_input = next(iter(bound_args.arguments.values()))
+#         else:
+#             raise ValueError("Function requires at least one argument, none provided")
+#         a: State[A, S] = State(state_input, fn)
+#         return a
+#
+#     return wrapper
